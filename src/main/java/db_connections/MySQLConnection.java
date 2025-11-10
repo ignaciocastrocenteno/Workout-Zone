9e41db4ae7636214d927ef190de0c2b52e9a7f23
Ignacio Castro Centeno <ignaciocastrocenteno@gmail.com>
2025-11-10 17:48:16 -0300

    Updating comment for the class 'MySQLConnection.java' for thorough explanation of how the DB connection works

--- src/main/java/db_connections/MySQLConnection.java
+++ src/main/java/db_connections/MySQLConnection.java
@@ -17,7 +17,7 @@ public class MySQLConnection {
             Here the information needed to establish a connection to a database engine. This information is found in
             'Session' panel in MySQL Workbench.
          */
-            // The name of the database we are going to use.
+            // The name of the database we are going to use (have to be previously created)
             String dbName = "workout_zone_db";
 
             /*
@@ -26,27 +26,34 @@ public class MySQLConnection {
                         unchanged, the server's default name will be shown in this field.
                 - Host: The name of the computer we're connecting to, which can be a personal computer or a server.
                         The host can be referenced with a name or with an IP address (e.g. the IP address for the
-                        localhost is 127.0.0.1 and would be the same to say 'localhost' than this IP address).
-                - Port: The specific port where the server is exposing the database/s to work on. It functions as
-                        external gateway to connect to the server and then accessing the visible databases.
-                - Login user: The user that's currently logged in working on the schemas. The privileges for each
-                              user are going to be different, so they could access different levels of depth or
-                              content that are available inside the database but aren't visible for all users.
+                        localhost is 127.0.0.1 and would be the same to say 'localhost' than this IP address. If
+                        we need to connect to a web server hosted on the internet, we'll require its IP address or
+                        a DNS).
+                - Port: The specific port where the server is exposing the database/s to work with. It functions as
+                        external gateway to connect to the server and then accessing the visible databases. If there're
+                        other databases that aren't to be visible, the database engine has to be configured for that
+                        purpose. Otherwise, whether we try to connect to a valid and existing DB that would be possible.
+                - Login user: The user that's currently logged in working on the schemas, or any other user with valid
+                              credentials. The privileges for each user are going to be different, so they could access
+                              different levels of depth or content that are available inside the database but aren't
+                              visible for all users (authentication/authorization concept & user roles)
                  */
             String serverURL = "jdbc:mysql://localhost:3306/" + dbName;
             String user = "root";
             String password = "admin";
 
-            // The connection process to the database could potentially throw exceptions, so we wrap the code in a try/catch block.
+            // The connection process to the database could potentially throw exceptions, so we wrap the code into a try/catch block.
             try {
                 /*
                     To start the connection process to the database through the server, we have to load in memory
                     the corresponding MySQL dependency class to begin the execution. In this case, for connections to
                     MySQL databases, we need to load the class 'Driver' to the memory. Each database vendor has its
-                    own and specific class to initialize the connection.
+                    own and specific class to initialize the database connection. The class is always included within
+                    the dependency we had previously defined onto the 'pom.xml' file.
                  */
-                Class.forName("com.mysql.cj.jdbc.Driver"); // The class to load in memory to start the connection
-                connection = DriverManager.getConnection(serverURL, user, password);
+                // The static method 'Class.forName()' allow us to load a specific class on memory
+                Class.forName("com.mysql.cj.jdbc.Driver"); // We load in memory the class that allows the DB connection
+                connection = DriverManager.getConnection(serverURL, user, password); // The DB connection intent itself
 
             } catch (Exception DBConnectionException) {
                 System.out.println("There was an exception while trying to connect to the database!: " +
